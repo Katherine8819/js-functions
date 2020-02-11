207eaae1512bf4f1d20a923c500e90fee377aae5
Katherine Zhdanova <katherine.zhdanova8@gmail.com>
2020-02-11 12:33:42 +0200

    second

--- js/02_factorial.js
+++ js/02_factorial.js
@@ -1,6 +1,15 @@
-(function () {
-    'use strict'
-})
+
+function getFactoriasl(number) {
+    let factorial = 1;
+
+    for (let i = 1; i <= number; i++) {
+        factorial *= i;
+    }
+    return factorial
+}
+
+console.log(getFactoriasl(8))
+
 
 
 // Написать функцию, которая вычисляет факториал переданного ей числа.
