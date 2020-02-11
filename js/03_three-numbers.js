2ac4ee706c211a3a00126f471ccb0449c9c3c968
Katherine Zhdanova <katherine.zhdanova8@gmail.com>
2020-02-11 12:42:28 +0200

    third

--- js/03_three-numbers.js
+++ js/03_three-numbers.js
@@ -1,6 +1,7 @@
-(function () {
-    'use strict'
-})
+function addNumbers(firstNumber, secondNumber, thirdNumber) {
+    return firstNumber.toString() + secondNumber.toString() + thirdNumber.toString()
+}
 
+console.log(addNumbers(3, 1, 8))
 
 // Написать функцию, которая принимает три отдельные цифры и превращает их в одно число. Например: цифры 1, 4, 9 превратятся в число 149.
