6612781f8d0ed0fda6251d2daebd3e37c03e28bf
Katherine Zhdanova <katherine.zhdanova8@gmail.com>
2020-02-11 12:49:32 +0200

    fourth

--- js/04_square.js
+++ js/04_square.js
@@ -1,6 +1,14 @@
-(function () {
-    'use strict'
-})
+function getSquare(width, height) {
+    let square = 0;
 
+    if (height == undefined) {
+        square = width * width
+    } else {
+        square = width * height
+    }
 
+    return square;
+}
+
+console.log(getSquare(5))
 // Написать функцию, которая принимает длину и ширину прямоугольника и вычисляет его площадь. Если в функцию передали 1 параметр, то она вычисляет площадь квадрата.
